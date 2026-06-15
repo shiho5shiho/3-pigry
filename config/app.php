629ae7c4b76dcf00f3944f2036d9bd1570dd4b56
shiho5shiho <shiho5shiho@gmail.com>
2026-06-15 21:14:21 +0900

    feat: 体重認証(Fortify)実装 #3

--- config/app.php
+++ config/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
+
+
 return [
 
     /*
@@ -168,6 +170,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class, // 追加
+
     ])->toArray(),
 
     /*
